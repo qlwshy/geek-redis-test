697244ffdb92668b52ce68276941af91b0bc6b8b
Augus <wen.bin.ou.yang@ibm.com>
2022-08-26 13:21:44 +0800

    First commit

---
+++ src/main/java/com/augus/geekredistest/GeekRedisTestApplication.java
@@ -0,0 +1,13 @@
+package com.augus.geekredistest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeekRedisTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeekRedisTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/augus/geekredistest/GeekRedisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.augus.geekredistest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekRedisTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
